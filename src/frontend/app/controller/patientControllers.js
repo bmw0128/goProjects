42afdb36f865868a48afb9664cfa989582078b60
Brian Wilson <brianwilson0128@gmail.com>
2015-03-28 17:46:29 -0700

    Add Patient working

--- src/frontend/app/controller/patientControllers.js
+++ src/frontend/app/controller/patientControllers.js
@@ -56,9 +56,8 @@ function PatientEditCtrl($scope, $location, Restangular, patient) {
         }
 
         if(!$scope.hasValidationErrors){
-            console.log("*** saving edited patient: " + JSON.stringify($scope.patient));
+            //console.log("*** saving edited patient: " + JSON.stringify($scope.patient));
             var s = getPatientJSONPOST($scope);
-            //var s = JSON.stringify($scope.patient);
 
             Restangular.all('patients/edit').post(s).then(
                 function() {
@@ -89,72 +88,7 @@ function PatientEditCtrl($scope, $location, Restangular, patient) {
             }
         }
 
-
-
-
-        /*
-        //see if the drug is in deletedInteractions array, if so, delete it, and add it
-        //to the selectedDrug
-        var isInDeletedInteractionsArray= false;
-        var idxOfItem;
-        var existingId= '';
-        for(var i=0; i < deletedInteractions.length; i++){
-            if(deletedInteractions[i].drugName === typeAheadDrugName){
-                isInDeletedInteractionsArray= true;
-                idxOfItem= i;
-                existingId= deletedInteractions[i].id;
-                break;
-            }
-        }
-        if(isInDeletedInteractionsArray){
-            deletedInteractions.splice(idxOfItem, 1);
-        }
-
-        $scope.selectedDrug.interactions.push({'id': '', 'drugName':'','severityLevel':'','summary':''});
-        $scope.size= $scope.selectedDrug.interactions.length;
-        $scope.selectedDrug.interactions[$scope.size - 1].drugName= typeAheadDrugName;
-        $scope.selectedDrug.interactions[$scope.size - 1].id= existingId;
-        $scope.selectedDrug.interactions.reverse();
-        */
     };
-
-
-
-
-
-/*
-    $scope.diseaseCheckboxClick= function(idx){
-
-        var cbId= '#cb-' + idx;
-        var isChecked= $(cbId).is(':checked');
-
-        if(isChecked)
-            $(cbId).attr('checked', true);
-        else
-            $(cbId).attr('checked', false);
-
-        isChecked= $(cbId).is(':checked');
-        var diseaseId= '#disease-' + idx;
-        var diseaseValueId= $(diseaseId).val();
-
-        if(!isChecked){
-            for(var i=0; i < $scope.selectedDiseases.length; i++){
-                var disease= $scope.selectedDiseases[i];
-                if(diseaseValueId === disease.id){
-                    $scope.selectedDiseases.splice(i, 1);
-                }
-            }
-        }else {
-            for (var j = 0; j < $scope.diseases.length; j++) {
-                var dis= $scope.diseases[j];
-                if(dis.id === diseaseValueId){
-                    $scope.selectedDiseases.push(dis);
-                }
-            }
-        }
-
-    };
-*/
 
     $scope.delete = function () {
 
@@ -163,38 +97,6 @@ function PatientEditCtrl($scope, $location, Restangular, patient) {
                 $location.path('/patients');
             });
     };
-
-    /*
-    $scope.searchField= function(){
-
-        for (var i = 0; i < $scope.selectedDiseases.length; i++) {
-            var outerDiseaseId = $scope.selectedDiseases[i].id;
-            //console.log("*** sf outerDiseaseId: " + outerDiseaseId);
-            var diseasesCopy= [];
-            for (var j = 0; j < $scope.diseases.length; j++) {
-                diseasesCopy.push($scope.diseases[j]);
-                //var diseaseId = "disease-" + j;
-                var diseaseElem= '#disease-' + j;
-                var diseaseIdValue = $(diseaseElem).val();
-                if (outerDiseaseId === diseaseIdValue) {
-                    //var cbId = "cb-" + j;
-                    //document.getElementById(cbId).checked = true;
-                    var cbId= '#cb-' + j;
-                    $(cbId).attr('checked', true);
-                    break;
-                }
-            }
-        }
-        if($('#searchField').val() === ''){
-            console.log("*** doing empty...");
-            var diseasesCopy= [];
-            for (var j = 0; j < $scope.diseases.length; j++) {
-                diseasesCopy.push($scope.diseases[j]);
-            }
-            $scope.diseases= diseasesCopy;
-        }
-    };
-    */
 
     $scope.removeDisease= function(id){
 
@@ -225,22 +127,6 @@ function PatientEditCtrl($scope, $location, Restangular, patient) {
             }
             $scope.patient.diseases= diseaseOjects;
             $scope.doPageLoad= false;
-            /*
-            for (var i = 0; i < $scope.patient.diseases.length; i++) {
-                var outerDiseaseId = $scope.patient.diseases[i];
-                for (var j = 0; j < $scope.diseases.length; j++) {
-                    var diseaseId = "disease-" + j;
-                    var diseaseIdValue = document.getElementById(diseaseId).value;
-                    if (outerDiseaseId === diseaseIdValue) {
-                        $scope.selectedDiseases.push($scope.diseases[j]);
-                        var cbId = "cb-" + j;
-                        document.getElementById(cbId).checked = true;
-                        break;
-                    }
-                }
-            }
-            $scope.doPageLoad= false;
-            */
         }
         return false;
     };
@@ -256,6 +142,20 @@ function PatientsCtrl($scope, Restangular) {
 }
 
 function PatientNewCtrl($scope, Restangular, $location, $http, $q) {
+
+    var patient=
+        {id:null,
+        firstName: null,
+        lastName: null,
+        age: null,
+        gender: null,
+        pregnant: null,
+        race: null,
+        bp: null,
+        heartRate: null,
+        diseases: []};
+
+    $scope.patient= patient;
 
     Restangular.all('diseases').getList().then(
         function(diseases) {
@@ -288,28 +188,36 @@ function PatientNewCtrl($scope, Restangular, $location, $http, $q) {
                 function() {
                     $location.path('/patients');
                 });
-
         }
-
     };
 
     setUpSections($scope);
+
+    $scope.addDisease= function(obj){
+
+        $scope.modeldisplay= '';
+        $scope.showNewInteractionDrug= true;
+        var typeAheadDrugName= obj.target.attributes.data.value;
+
+        //console.log("*** addDisease name: %s", typeAheadDrugName);
+
+        if($scope.patient.diseases == null){
+            $scope.patient.diseases= [];
+        }
+
+        for (var j = 0; j < $scope.diseases.length; j++) {
+            var diseaseName = $scope.diseases[j].diseaseName;
+            if (typeAheadDrugName.trim() === diseaseName) {
+                //console.log("*** adding disease from typeahead: %s", JSON.stringify($scope.diseases[j]));
+                $scope.patient.diseases.push($scope.diseases[j]);
+                break;
+            }
+        }
+    };
 }
 
 function getSelectedDiseases($scope){
-    /*
-    var objs= [];
-    var size= $scope.diseases.length;
-    for(var i=0; i < size; i++){
-        var diseaseId= "disease-" + i;
-        var cbId= "cb-" + i;
-        if(document.getElementById(cbId).checked){
-            var diseaseIdVal= document.getElementById(diseaseId).value;
-            objs.push(diseaseIdVal);
-        }
-    }
-    return objs;
-    */
+
     var objs= [];
     for(var i=0; i < $scope.patient.diseases.length; i++){
       objs.push($scope.patient.diseases[i].id);
